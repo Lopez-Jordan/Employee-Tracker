7b1f8123b3e874c889c18544dd178f65c7ad3856
Jordan Lopez <jordanlopezemail@gmail.com>
2023-08-02 19:12:11 -0700

    working inquirer and prompt user

--- index.js
+++ index.js
@@ -6,29 +6,33 @@ init();
 async function init(){
     while (true){
         let data = await promptUser();
+        console.log(data);
         if(data.action === "quit"){
             break;
         }
         else if (data.action === "view all departments"){
-            // do something
+            console.log("works");
         }
         else if (data.action === "view all roles"){
-            // do something 
+            console.log("works");
         }
         else if (data.action === "view all employees"){
-            // do something
+            console.log("works");
         }
         else if (data.action === "add a department"){
-            // do something
+            console.log("works");
         }
         else if (data.action === "add a role"){
-            // do asdfasdf
+            console.log("works");
         }
         else if (data.action === "add an employee"){
-
+            console.log("works");
+        }
+        else if (data.action === "update an employee role"){
+            console.log("works")
         }
         else{
-            // "update an employee role"
+            console.log("quit");
         }
     
             
@@ -40,7 +44,18 @@ async function init(){
 };
 
 
-
+async function promptUser(){
+    const answers = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'action',
+            message: "What would you like to choose?",
+            choices: ["view all departments", "view all roles", 
+            "view all employees", "add a department", "add a role", "add an employee", "update an employee role","quit"]
+        }
+    ]);
+    return answers;
+}
 // prompt user
 // use that data and perform some sort of action in the db
 //
